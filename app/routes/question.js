eee528f78742ff774c6ba2ffdedd3d8cc7da2b27
Ryan Apking <ryan.apking@gmail.com>
2016-10-28 09:12:58 -0700

    add question-details component, and correct question model to retrieve data correctly from firebase.

--- app/routes/question.js
+++ app/routes/question.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.query('question', {
-      id: params.question_id
-    });
+    return this.store.findRecord('question', params.question_id);
   }
 });
