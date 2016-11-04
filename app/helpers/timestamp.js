0d09b7484ec75ab57e2f8160f73ad85294bef5da
Ryan Apking <ryan.apking@gmail.com>
2016-11-04 16:56:17 -0700

    added missing semicolon to timestamp helper.

--- app/helpers/timestamp.js
+++ app/helpers/timestamp.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function timestamp(params/*, hash*/) {
   // using moment instead of this helper would be more efficient and offer more flexibility, but this demonstrates basic use of a helper
   var timestamp = params[0];
-  var date = new Date(timestamp)
+  var date = new Date(timestamp);
   return Ember.String.htmlSafe('<span>' + date.toString().slice(4, 24) + '</span>');
 }
 
