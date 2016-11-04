ff98d60359f8d2952c7774002dd51992673d6cf6
Ryan Apking <ryan.apking@gmail.com>
2016-11-04 14:49:03 -0700

    added helper to display a timestamp when given a unix date.

---
+++ tests/unit/helpers/timestamp-test.js
@@ -0,0 +1,10 @@
+import { timestamp } from 'answer-board/helpers/timestamp';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | timestamp');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = timestamp([42]);
+  assert.ok(result);
+});
